12afd0dfb62abb56fae91314056579579cc07151
Abriel Cleaver <abrielcleaver@gmail.com>
2021-08-25 16:35:18 -0700

    add option to input pronouns

--- app.js
+++ app.js
@@ -11,11 +11,29 @@ const nameInput = document.getElementById('name-input');
 updateButton.addEventListener('click', () => {
     // post-click logic goes here
 
-    const name = textBox.value;
-console.log(name);
+const name = textBox.value;
+    console.log(name);
+    
     nameInput.textContent = name ; 
 });
 
+const updateButtonP = document.getElementById('update-button-p');
+    console.log(updateButtonP);
+        // get an input
+const textBoxP = document.getElementById('text-box-p');
+    console.log(textBoxP);
+
+const pronounInput = document.getElementById('pronoun-input');
+    console.log(pronounInput);
+
+updateButtonP.addEventListener('click', () => {
+    // post-click logic goes here
+
+    const pronouns = textBoxP.value;
+        console.log(pronouns);
+        
+        pronounInput.textContent = pronouns ;
+})
 
 
 
