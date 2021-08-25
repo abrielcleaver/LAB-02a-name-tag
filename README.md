c1f5f085ee703952910c0ba63b0ea523294d1e04
Abriel Cleaver <abrielcleaver@gmail.com>
2021-08-25 13:20:14 -0700

    add text box and update button

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LAB02b-name-tag
+
+Recreate a classic name tag in HTML
+
---
+++ app.js
@@ -0,0 +1,12 @@
+const updateButttonN = document.getElementById('update-button-n');
+
+const name = document.getElementById('name');
+
+
+
+
+
+
+
+
+
